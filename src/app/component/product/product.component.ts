5eb03f1038b7fef1ffbe8b92067ebf1de50da14a
mohammedsayis <mohammedsayis@gmail.com>
2023-02-18 10:52:58 +0530

    cart card Design is completed

---
+++ src/app/component/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+  public productList : any ;
+  constructor(private api : ApiService){}
+  ngOnInit():void {
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+    })
+  }
+
+}
